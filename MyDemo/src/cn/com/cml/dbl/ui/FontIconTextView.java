bb5e65ec2e0988fb4d1406662bb72fc1e7d3dd28
menglin.chen@team-lab.cn <menglin.chen@team-lab.cn>
2014-11-13 17:45:44 +0800

    ddddd ss

---
+++ MyDemo/src/cn/com/cml/dbl/ui/FontIconTextView.java
@@ -0,0 +1,28 @@
+package cn.com.cml.dbl.ui;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class FontIconTextView extends TextView {
+
+	public FontIconTextView(Context context) {
+		super(context);
+		setTypeface(Typeface.createFromAsset(context.getAssets(),
+				"font/fontello.ttf"));
+	}
+
+	public FontIconTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		setTypeface(Typeface.createFromAsset(context.getAssets(),
+				"font/fontello.ttf"));
+	}
+
+	public FontIconTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setTypeface(Typeface.createFromAsset(context.getAssets(),
+				"font/fontello.ttf"));
+	}
+
+}
