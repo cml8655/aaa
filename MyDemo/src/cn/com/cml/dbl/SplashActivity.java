2cc88562ff3b18753933a4b559bbd91a100652a2
menglin.chen@team-lab.cn <menglin.chen@team-lab.cn>
2014-12-05 18:09:39 +0800

    upate

--- MyDemo/src/cn/com/cml/dbl/SplashActivity.java
+++ MyDemo/src/cn/com/cml/dbl/SplashActivity.java
@@ -3,20 +3,29 @@ package cn.com.cml.dbl;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 
 import cn.com.cml.dbl.util.AppUtil;
 import cn.com.cml.dbl.util.PrefUtil_;
 import android.app.Activity;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 @EActivity(R.layout.activity_splash)
 public class SplashActivity extends Activity {
+
+	@ViewById(R.id.splash_logo)
+	ImageView logoView;
 
 	@Pref
 	PrefUtil_ pref;
 
 	@AfterViews
 	protected void initConfig() {
+
+		logoView.startAnimation(AnimationUtils.loadAnimation(
+				getApplicationContext(), R.anim.splash_anim));
 
 		if (!pref.introduceVersion().exists()) {
 			// TODO 跳到欢迎界面
@@ -32,7 +41,7 @@ public class SplashActivity extends Activity {
 
 		// TODO 跳到登录界面
 
-		closeActivity();
+		// closeActivity();
 	}
 
 	@UiThread(delay = 300)
