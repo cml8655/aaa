c353617ca73ff2e39fcd96b406a27d31f48f3647
menglin.chen@team-lab.cn <menglin.chen@team-lab.cn>
2015-01-07 16:18:26 +0800

    update

--- MyDemo/src/cn/com/cml/dbl/net/ApiRequestServiceClient.java
+++ MyDemo/src/cn/com/cml/dbl/net/ApiRequestServiceClient.java
@@ -8,6 +8,7 @@ import org.androidannotations.annotations.RootContext;
 import org.json.JSONArray;
 
 import android.content.Context;
+import android.os.Build;
 import cn.bmob.v3.BmobPushManager;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.BmobUser;
@@ -27,6 +28,7 @@ import cn.com.cml.dbl.mode.api.Suggestion;
 import cn.com.cml.dbl.mode.api.User;
 import cn.com.cml.dbl.model.PushModel;
 import cn.com.cml.dbl.util.CommonUtils;
+import cn.com.cml.dbl.util.DeviceUtil;
 
 import com.google.gson.Gson;
 
@@ -99,6 +101,8 @@ public class ApiRequestServiceClient implements ApiRequestService {
 				bindUpdate.setBindType(MobileBind.TYPE_BIND);
 				bindUpdate.setBindPassword(pass);
 				bindUpdate.setImei(deviceId);
+				bindUpdate
+						.setDeviceName(Build.MANUFACTURER + "-" + Build.MODEL);
 
 				// 重新绑定
 				bindUpdate.save(context, listener);
--- MyDemo/src/cn/com/cml/dbl/view/UserInfoFragment.java
+++ MyDemo/src/cn/com/cml/dbl/view/UserInfoFragment.java
@@ -1,22 +1,34 @@
 package cn.com.cml.dbl.view;
 
+import java.util.List;
+
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.FragmentArg;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.widget.TextView;
+import cn.bmob.v3.BmobUser;
 import cn.com.cml.dbl.LoginActivity_;
-import cn.com.cml.dbl.MainActivity;
 import cn.com.cml.dbl.R;
+import cn.com.cml.dbl.listener.BaseFindListener;
+import cn.com.cml.dbl.mode.api.MobileBind;
+import cn.com.cml.dbl.mode.api.User;
+import cn.com.cml.dbl.net.ApiRequestServiceClient;
 import cn.com.cml.dbl.util.DialogUtil;
 import cn.com.cml.dbl.view.DefaultDialogFragment.OnItemClickListener;
 
 @EFragment(R.layout.fragment_userinfo)
 public class UserInfoFragment extends Fragment implements OnItemClickListener {
+
+	private static final String INIT_CHARACTOR = "--";
+
+	private DialogFragment dialog;
 
 	@ViewById(R.id.userinfo_account)
 	TextView accountView;
@@ -27,18 +39,47 @@ public class UserInfoFragment extends Fragment implements OnItemClickListener {
 	@ViewById(R.id.userinfo_device)
 	TextView deviceView;
 
+	@Bean
+	ApiRequestServiceClient apiClient;
+
 	@AfterViews
 	public void afterView() {
 
-		accountView.setText(getString(R.string.userinfo_account, 1));
-		pointView.setText(getString(R.string.userinfo_point, 2));
-		deviceView.setText(getString(R.string.userinfo_unbind_device));
+		dialog = DialogUtil.dataLoadingDialog();
+		dialog.show(getFragmentManager(), "user_info");
+
+		User user = BmobUser.getCurrentUser(getActivity(), User.class);
+
+		accountView.setText(getString(R.string.userinfo_account,
+				user.getUsername()));
+		pointView.setText(getString(R.string.userinfo_point, user.getScore()));
+		deviceView.setText(getString(R.string.userinfo_bind_device,
+				INIT_CHARACTOR));
+
+		apiClient.bindDeviceQuery(new BaseFindListener<MobileBind>(
+				getActivity()) {
+			@Override
+			public void onFinish() {
+				super.onFinish();
+				dialog.dismiss();
+			}
+
+			@Override
+			public void onSuccess(List<MobileBind> result) {
+
+				if (result.size() > 0) {
+					String deviceName = result.get(0).getDeviceName();
+					setBindDevice(deviceName);
+				}
+
+			}
+		});
+
 	}
 
-	@Click(R.id.userinfo_logout)
-	protected void onLogoutClicked() {
-		DialogUtil.defaultDialog(R.string.exit_confirm, 1, this).show(
-				getFragmentManager(), "logout");
+	@UiThread
+	protected void setBindDevice(String device) {
+		deviceView.setText(getString(R.string.userinfo_bind_device, device));
 	}
 
 	@Override
